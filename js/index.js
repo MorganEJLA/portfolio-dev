bb00faa3617831abdc0ecf96ee61d182d7d1f383
MorganEJLA <mojones311@gmail.com>
2026-04-24 18:08:34 -0400

    updated spelling of WP

--- js/index.js
+++ js/index.js
@@ -37,7 +37,7 @@
   const typedText = document.getElementById("typed-text");
   if (typedText) {
     const words = [
-      "Wordpress",
+      "WordPress",
       "React",
       "PHP",
       "JavaScript",
